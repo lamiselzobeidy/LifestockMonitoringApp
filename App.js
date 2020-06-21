b311c54ce239d8848a14b51d0b41f07309ccc57f
nourfayed <nourfayedd@gmail.com>
2020-06-21 21:41:36 +0200

    convert unix time to normal

--- App.js
+++ App.js
@@ -3,45 +3,59 @@ import { StyleSheet, Text, View , FlatList} from 'react-native';
 
 
 
+const getTime=  (item)=> {
+  
+    let milliseconds = item.timestamp * 1000 // 1575909015000
+    let dateObject = new Date(milliseconds)
+    let humanDateFormat = dateObject.toLocaleString() //2
 
+
+return <Text> {humanDateFormat}</Text>
+}
 
 export default function App() {
   const [x, setx] = useState(-2);
   const [arr, setarr] = useState([]);
+
   const  getlastvalue = () => {
-    let x;
     fetch('https://things.ubidots.com/api/v1.6/devices/flaskServer/posture-value/lv/?token=BBFF-BjBE8UN70vxuFasG3L0PVLIxv0SNg6')
     .then(response => response.json())
     .then(data => {
       console.log(data)
-      setx(data.results)
+      setx(data)
     });
-  
   }
   const  getallvalues = () => {
-    let x;
     fetch('https://things.ubidots.com/api/v1.6/devices/flaskServer/posture-value/values/?token=BBFF-BjBE8UN70vxuFasG3L0PVLIxv0SNg6')
     .then(response => response.json())
     .then(data => {
       console.log(data.results)
-      setarr(data.results)
-      console.log(arr);
+     // arr=data.results
+       setarr(data.results)
+       console.log(arr);
     });
   
   }
+
   useEffect(()=>{
     getlastvalue()  
     getallvalues()
  },[]);
-  if(! arr.length) return <view> betengan </view>
+  
+  
   return (
     
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! {x}</Text>   
+     
+      <Text>Current posture is  {x}</Text>   
       <FlatList
             data={arr}  
             renderItem={ ( {item} )=>(
-                <Text>{item.value}</Text>
+                
+                <View>
+                  <Text>{item.value}</Text>
+                  <Text> {getTime(item)}</Text>
+                </View>
                 )}
             // keyExtractor={(item) => item.id.toString()} 
             />
