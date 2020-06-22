ecb12ca119671363e0dd05eaf3f585d237fd14ab
Lamis Elzobeidy <lamis.zobeidy@gmail.com>
2020-06-22 19:02:17 +0200

    Current posture screen

--- Tabs.js
+++ Tabs.js
@@ -3,22 +3,38 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import CurrentPosture from './screens/CurrentPosturePage';
 import DailyReport from './screens/DailyReportPage';
+import { createStackNavigator } from '@react-navigation/stack';
+import { FontAwesome, Octicons, AntDesign } from "@expo/vector-icons";
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
-
+const Stack = createStackNavigator();
 
 const Tab = createMaterialBottomTabNavigator();
+
+// const createHomeStack = () => (
+//   <Stack.Navigator screenOptions={{
+//     headerTintColor: 'white',
+//     headerTitleAlign: 'center',
+//     headerStyle: { backgroundColor: '#b504be'},
+//     headerTitleStyle:{ fontWeight:'bold', fontSize:24}
+//   }}
+//   name="Life Stock Monitoring">
+//   </Stack.Navigator>
+// );
+
 
 function MyTabs() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Current Posture" component={CurrentPosture} />
-      <Tab.Screen name="Daily Report" component={DailyReport} />
+      <Tab.Screen options={{ tabBarIcon: ({ focused, color }) => ( <Octicons name="package" size={25} color={color} /> ), }} screenOptions={{headerTitleAlign:'left'}} name="Current Posture" component={CurrentPosture}/>
+      <Tab.Screen options={{ tabBarIcon: ({ focused, color }) => ( <Octicons name="package" size={25} color={color} /> ), }} name="Daily Report" component={DailyReport} />
     </Tab.Navigator>
   );
 }
 export default function App() {
     return (
       <NavigationContainer>
+        
         <MyTabs />
       </NavigationContainer>
     );
--- screens/CurrentPosturePage.js
+++ screens/CurrentPosturePage.js
@@ -1,9 +1,83 @@
-import React from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
+
+const getPostureName = (value) => {
+    if (value == 0) return 'Standing'
+    else if (value == 1) return 'Resting'
+    else if (value == 2) return 'Moving'
+    return 'Eating'
+}
 
 export default function CurrentPosture() {
-    return(
-        <Text>hello from CurrentPosture</Text>
+
+    const [currentPosture, setCurrentPosture] = useState(0);
+
+    const getCurrentPosture = () => {
+        fetch('https://things.ubidots.com/api/v1.6/devices/flaskServer/posture-value/lv/?token=BBFF-BjBE8UN70vxuFasG3L0PVLIxv0SNg6')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                setCurrentPosture(data)
+            });
+    }
+    return (
+        <View style={styles.container}>
+            <Image style={styles.image} source={require('../images/cow.png')} />
+            <View style={styles.postureContainer}>
+                <Text style={styles.posture}>Current Posture</Text>
+                <Text style={styles.postureValue}>{getPostureName(currentPosture)}</Text>
+            </View>
+            <Text style={styles.content}></Text>
+        </View>
     )
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 6,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        padding: '10px',
+        paddingTop: '100px'
+    },
+    image: {
+        flex: 2,
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        width: 310,
+        height: 100,
+        marginBottom: '20px',
+    },
+    postureContainer: {
+        flex: 2,
+        flexDirection: 'column',
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    posture: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize: 40,
+        textAlign: 'center',
+        fontFamily: 'roboto',
+        fontWeight: 'bold',
+    },
+    postureValue: {
+        flex:1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize: 25,
+        textAlign: 'center',
+    },
+    content: {
+        flex: 2,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+        textAlign: 'center',
+    },
+
+});
